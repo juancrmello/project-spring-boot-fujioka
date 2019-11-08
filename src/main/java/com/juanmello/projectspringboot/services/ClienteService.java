ed3fd86b8f1db4f6826595f198c281934545d52a
juancrmello <juanribeirofx@gmail.com>
2019-11-08 03:39:39 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/juanmello/projectspringboot/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.juanmello.projectspringboot.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juanmello.projectspringboot.domain.Cliente;
+import com.juanmello.projectspringboot.repositories.ClienteRepository;
+import com.juanmello.projectspringboot.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
