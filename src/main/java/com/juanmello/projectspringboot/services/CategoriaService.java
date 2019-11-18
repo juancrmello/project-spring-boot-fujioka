a6106c237f6fbdd34c98af0f012d57192ee582d1
juancrmello <juanribeirofx@gmail.com>
2019-11-17 22:52:09 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/juanmello/projectspringboot/services/CategoriaService.java
+++ src/main/java/com/juanmello/projectspringboot/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juanmello.projectspringboot.domain.Categoria;
+import com.juanmello.projectspringboot.domain.Cliente;
 import com.juanmello.projectspringboot.dto.CategoriaDTO;
 import com.juanmello.projectspringboot.repositories.CategoriaRepository;
 import com.juanmello.projectspringboot.services.exception.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update (Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -59,5 +61,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
