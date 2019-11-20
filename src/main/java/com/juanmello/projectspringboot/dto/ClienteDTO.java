9e4cf9a3db09d29b3236d258e76dab1a046ead00
juancrmello <juanribeirofx@gmail.com>
2019-11-20 16:56:40 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/juanmello/projectspringboot/dto/ClienteDTO.java
+++ src/main/java/com/juanmello/projectspringboot/dto/ClienteDTO.java
@@ -13,11 +13,12 @@ public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	@NotEmpty(message="Prrenchimento obrigatório")
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
-	@NotEmpty(message="Prrenchimento obrigatório")
+	@NotEmpty(message="Preenchimento obrigatório")
 	@Email(message="Email inválido")
 	private String email;
 	
