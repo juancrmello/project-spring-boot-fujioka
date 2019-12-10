8752d28f4c890688ff8ac81590e10ff96b1b9f75
juancrmello <juanribeirofx@gmail.com>
2019-12-10 01:05:06 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/juanmello/projectspringboot/ProjectspringbootApplication.java
+++ src/main/java/com/juanmello/projectspringboot/ProjectspringbootApplication.java
@@ -1,39 +1,18 @@
 package com.juanmello.projectspringboot;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.juanmello.projectspringboot.domain.Categoria;
-import com.juanmello.projectspringboot.domain.Cidade;
-import com.juanmello.projectspringboot.domain.Cliente;
-import com.juanmello.projectspringboot.domain.Endereco;
-import com.juanmello.projectspringboot.domain.Estado;
-import com.juanmello.projectspringboot.domain.ItemPedido;
-import com.juanmello.projectspringboot.domain.Pagamento;
-import com.juanmello.projectspringboot.domain.PagamentoComBoleto;
-import com.juanmello.projectspringboot.domain.PagamentoComCartao;
-import com.juanmello.projectspringboot.domain.Pedido;
-import com.juanmello.projectspringboot.domain.Produto;
-import com.juanmello.projectspringboot.domain.enums.EstadoPagamento;
-import com.juanmello.projectspringboot.domain.enums.TipoCliente;
-import com.juanmello.projectspringboot.repositories.CategoriaRepository;
-import com.juanmello.projectspringboot.repositories.CidadeRepository;
-import com.juanmello.projectspringboot.repositories.ClienteRepository;
-import com.juanmello.projectspringboot.repositories.EnderecoRepository;
-import com.juanmello.projectspringboot.repositories.EstadoRepository;
-import com.juanmello.projectspringboot.repositories.ItemPedidoRepository;
-import com.juanmello.projectspringboot.repositories.PagamentoRepository;
-import com.juanmello.projectspringboot.repositories.PedidoRepository;
-import com.juanmello.projectspringboot.repositories.ProdutoRepository;
+import com.juanmello.projectspringboot.services.S3Service;
 
 @SpringBootApplication
 public class ProjectspringbootApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectspringbootApplication.class, args);
 	}
@@ -41,6 +20,7 @@ public class ProjectspringbootApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3Service.uploadFile("C:\\temp\\fotos\\coringa.jpeg");
 	}
 
 }
